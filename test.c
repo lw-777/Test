30a6701a0758d357fc888d2d1fee8b761fe418f1
lw-777 <58110209+lw-777@users.noreply.github.com>
2019-12-22 18:57:38 +0800

    修改test.c文件
    第一次使用GitHub修改文件，试试水

--- test.c
+++ test.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
+/*
+
 int main(int argc, char **argv)
 {
     printf ("Wecome to GitHub!\n");
 
+    return 0;
+}
+
+*/
+
+int main(int argc, char **argv)
+{
+    printf ("Hello World!\n");
+    
     return 0;
 }
